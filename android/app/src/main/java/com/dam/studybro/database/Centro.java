4381095c9605f762046851c3da91af4822cf41f6
Antoniomba23 <antoniomba23@gmail.com>
2026-02-18 14:03:56 +0100

    Feat: Create Centro entity

---
+++ android/app/src/main/java/com/dam/studybro/database/Centro.java
@@ -0,0 +1,41 @@
+package com.dam.studybro.database;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.ColumnInfo;
+
+@Entity(tableName = "centros")
+public class Centro {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "nombre")
+    public String nombre;
+
+    @ColumnInfo(name = "ciudad")
+    public String ciudad;
+
+    @ColumnInfo(name = "direccion")
+    public String direccion;
+
+    @ColumnInfo(name = "web_url")
+    public String webUrl;
+
+    @ColumnInfo(name = "valoracion_media")
+    public float valoracionMedia;
+
+    @ColumnInfo(name = "codigo_api")
+    public String codigoApi;
+
+    // Constructor vacío requerido por Room
+    public Centro() {}
+
+    public Centro(String nombre, String ciudad, String direccion, String webUrl, float valoracionMedia, String codigoApi) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+        this.webUrl = webUrl;
+        this.valoracionMedia = valoracionMedia;
+        this.codigoApi = codigoApi;
+    }
+}
