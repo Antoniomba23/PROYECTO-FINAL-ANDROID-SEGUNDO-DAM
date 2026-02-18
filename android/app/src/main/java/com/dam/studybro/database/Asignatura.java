1cd1f45a309b251d6ed13d35ba0ce48ef8746204
Antoniomba23 <antoniomba23@gmail.com>
2026-02-18 14:04:21 +0100

    Feat: Create Asignatura entity

---
+++ android/app/src/main/java/com/dam/studybro/database/Asignatura.java
@@ -0,0 +1,33 @@
+package com.dam.studybro.database;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.ColumnInfo;
+import androidx.room.ForeignKey;
+
+@Entity(tableName = "asignaturas",
+        foreignKeys = @ForeignKey(entity = Especialidad.class,
+                                  parentColumns = "id",
+                                  childColumns = "especialidad_id",
+                                  onDelete = ForeignKey.CASCADE))
+public class Asignatura {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "nombre")
+    public String nombre;
+
+    @ColumnInfo(name = "curso")
+    public int curso;
+
+    @ColumnInfo(name = "especialidad_id")
+    public int especialidadId;
+
+    public Asignatura() {}
+
+    public Asignatura(String nombre, int curso, int especialidadId) {
+        this.nombre = nombre;
+        this.curso = curso;
+        this.especialidadId = especialidadId;
+    }
+}
