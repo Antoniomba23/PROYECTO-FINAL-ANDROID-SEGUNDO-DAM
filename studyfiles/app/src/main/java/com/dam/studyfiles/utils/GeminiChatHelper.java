b4bf602407c28eddf4c48b7010913ba70a59b180
Antonio MBA NZANG <antonio@mbanzang.com>
2026-05-15 00:55:45 +0200

    refactor: ocultar clave API de Gemini en local.properties usando BuildConfig

--- studyfiles/app/src/main/java/com/dam/studyfiles/utils/GeminiChatHelper.java
+++ studyfiles/app/src/main/java/com/dam/studyfiles/utils/GeminiChatHelper.java
@@ -22,7 +22,7 @@ import okhttp3.Response;
 
 public class GeminiChatHelper {
 
-    private static final String API_KEY = "AIzaSyAemK4le918ROYh2OyOIhPq1WYt6lx1zgI";
+    private static final String API_KEY = com.dam.studyfiles.BuildConfig.GEMINI_API_KEY;
     private static final String API_URL = "https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=" + API_KEY;
 
     public interface GeminiCallback {
