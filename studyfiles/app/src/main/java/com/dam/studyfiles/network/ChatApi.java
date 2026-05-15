d9db0504aae14a0df5320ac8963b19ce2b88be45
Antonio MBA NZANG <antonio@mbanzang.com>
2026-05-15 02:38:22 +0200

    fix: remover cabeceras duplicadas en ChatApi que causaban error 400

--- studyfiles/app/src/main/java/com/dam/studyfiles/network/ChatApi.java
+++ studyfiles/app/src/main/java/com/dam/studyfiles/network/ChatApi.java
@@ -3,37 +3,27 @@ package com.dam.studyfiles.network;
 import com.dam.studyfiles.models.MensajeChat;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ChatApi {
 
-    @Headers({
-        "apikey: " + SupabaseClient.API_KEY,
-        "Authorization: Bearer " + SupabaseClient.API_KEY
-    })
     @GET("rest/v1/mensajes_chat?select=*&order=fecha.asc")
     Call<List<MensajeChat>> getHistorialChat(@Query("usuario_id") String eqUsuarioId);
 
     @Headers({
-        "apikey: " + SupabaseClient.API_KEY,
-        "Authorization: Bearer " + SupabaseClient.API_KEY,
-        "Content-Type: application/json",
         "Prefer: return=representation"
     })
     @POST("rest/v1/mensajes_chat")
     Call<List<MensajeChat>> insertarMensaje(@Body MensajeChat mensaje);
 
-    @Headers({
-        "apikey: " + SupabaseClient.API_KEY,
-        "Authorization: Bearer " + SupabaseClient.API_KEY,
-        "Content-Type: application/json"
-    })
     @PATCH("rest/v1/mensajes_chat")
     Call<Void> migrarMensajes(@Query("usuario_id") String eqUsuarioIdAntiguo, @Body Map<String, Object> body);
 }
