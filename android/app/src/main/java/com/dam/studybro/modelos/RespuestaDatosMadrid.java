4002ca442a3ae7acab0a47c54e771592802d78e9
Antonio MBA NZANG <antonio@mbanzang.com>
2026-05-22 04:29:25 +0200

    Limpieza del repositorio: eliminacion de archivos del proyecto antiguo StudyBro

--- android/app/src/main/java/com/dam/studybro/modelos/RespuestaDatosMadrid.java
+++
@@ -1,9 +0,0 @@
-package com.dam.studybro.modelos;
-
-import com.google.gson.annotations.SerializedName;
-import java.util.List;
-
-public class RespuestaDatosMadrid {
-    @SerializedName("@graph")
-    public List<CentroMadrid> graph;
-}
